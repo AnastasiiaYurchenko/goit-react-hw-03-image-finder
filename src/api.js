99779816ec3c6473e0c5deb2391073ef70d0db4e
AnastasiiaYurchenko <nastya.brok@ukr.net>
2023-05-05 00:40:59 +0300

    fix api

--- src/api.js
+++ src/api.js
@@ -1,8 +1,14 @@
-// import axios from 'axios';
+import axios from 'axios';
 
-// export const fetchPictures = async () => {
-//   const response = await axios.get(
-//     'https://pixabay.com/api/?q=cat&page=1&key=34416785-706900f4c4344fdefb158122c&image_type=photo&orientation=horizontal&per_page=12'
-//   );
-//   return response.data;
-// };
+axios.defaults.baseURL = 'https://pixabay.com/api';
+
+export const fetchPictures = async (searchName, page) => {
+  const API_KEY = '34416785-706900f4c4344fdefb158122c';
+  const response = await axios.get(
+    `/?q=${searchName}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+  );
+  if (response.status === 404) {
+    throw new Error('Something went wrong, please try again', response.status);
+  }
+  return response.data;
+};
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import { Searchbar } from './Searchbar/Searchbar';
 import { Button } from './Button/Button';
 import { Component } from 'react';
 import { Loader } from './Loader/Loader';
-import axios from 'axios';
+import * as API from '../api';
 
 export const ERROR_MSG = 'Something went wrong, please try again';
 
@@ -29,13 +29,14 @@ export class App extends Component {
       this.setState({ loading: true, error: null });
 
       try {
-        const fetchedPictures = await axios.get(
-          `https://pixabay.com/api/?q=${this.state.searchName}&page=${this.state.page}&key=34416785-706900f4c4344fdefb158122c&image_type=photo&orientation=horizontal&per_page=12`
+        const fetchedPictures = await API.fetchPictures(
+          this.state.searchName,
+          this.state.page
         );
 
         this.setState(prevState => {
           return {
-            pictures: [...prevState.pictures, ...fetchedPictures.data.hits],
+            pictures: [...prevState.pictures, ...fetchedPictures.hits],
           };
         });
       } catch (error) {
